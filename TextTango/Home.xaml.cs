4d3af1e42b0568516dceaa1ce36bcb698d8a1243
MarusovaOlesya <marusovaolesya00@gmail.com>
2024-04-18 11:47:52 +0300

    aa

---
+++ TextTango/Home.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TextTango
+{
+    /// <summary>
+    /// Логика взаимодействия для Home.xaml
+    /// </summary>
+    public partial class Home : Window
+    {
+        private SqlConnection bd;
+        public string Id;
+        public Home(SqlConnection bd, string Id)
+        {
+            InitializeComponent();
+            this.bd = bd;
+            this.Id = Id;
+            WriteName();
+        }
+
+        private void WriteName()
+        {
+            SqlCommand sqlCommand = new SqlCommand($"select Name from Users where Id = {Id}", bd);
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+            reader.Read();
+            Name.Text = reader[0].ToString();
+            reader.Close();
+        }
+
+        private Grid Chats()
+        {
+            Grid grid = new Grid();
+
+            Button button = new Button();
+            button.Height = 50;
+            button.Width = 170;
+            button.HorizontalAlignment = HorizontalAlignment.Left;
+            button.Background = Brushes.White;
+            button.Foreground = Brushes.White;
+
+            Style style = new Style(typeof(Border));
+            style.Setters.Add(new Setter(Border.CornerRadiusProperty, new CornerRadius(10)));
+
+            button.Resources.Add(typeof(Border), style);
+
+            Grid innerGrid = new Grid();
+            innerGrid.HorizontalAlignment = HorizontalAlignment.Center;
+            innerGrid.Width = 167;
+
+            StackPanel stackPanel = new StackPanel();
+            stackPanel.Orientation = Orientation.Horizontal;
+
+            Border border = new Border();
+            border.Height = 40;
+            border.Width = 40;
+            border.CornerRadius = new CornerRadius(30);
+            border.BorderThickness = new Thickness(1);
+            border.BorderBrush = Brushes.Black;
+
+            Image image = new Image();
+            image.Source = new BitmapImage(new Uri("c:\\users\\admin\\desktop\\texttango\\texttango\\images\\icons\\2.png"));
+
+            border.Child = image;
+            stackPanel.Children.Add(border);
+
+            TextBlock textBlock = new TextBlock();
+            textBlock.Text = "pinkrabbit";
+            textBlock.FontSize = 20;
+            textBlock.Foreground = Brushes.Black;
+            textBlock.HorizontalAlignment = HorizontalAlignment.Left;
+            textBlock.VerticalAlignment = VerticalAlignment.Center;
+            textBlock.Margin = new Thickness(45, 0, 0, 0);
+
+            innerGrid.Children.Add(stackPanel);
+            innerGrid.Children.Add(textBlock);
+
+            button.Content = innerGrid;
+
+            grid.Children.Add(button);
+
+            Content = grid;
+            return grid;
+        }
+    }
+}
--- TextTango/MainWindow.xaml.cs
+++ TextTango/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,38 @@ namespace TextTango
     /// </summary>
     public partial class MainWindow : Window
     {
+        private SqlConnection bd;
         public MainWindow()
         {
+            bd = new SqlConnection(@"Data Source=3205EC12; Initial Catalog=TextTango; Integrated Security=True");
+            bd.Open();
             InitializeComponent();
+        }
+
+        private void LoginIn_Clik(object sender, RoutedEventArgs e)
+        {
+            string cm = "select Id from Users where Login = '"+Login.Text+"' and Password = '"+Password.Text+"'";
+            SqlCommand cmd = new SqlCommand(cm,bd);
+            SqlDataReader rdr = cmd.ExecuteReader();
+            if (rdr.Read())
+            {
+                var Id = rdr[0].ToString();
+                rdr.Close();
+                Home home = new Home(bd, Id);
+                home.Show();
+                this.Close();
+            }
+            else
+            {
+                Text.Text = "ERROR";
+            }
+        }
+
+        private void RegisternNow_Click(object sender, RoutedEventArgs e)
+        {
+            Register register = new Register();
+            register.Show();
+            this.Close();
         }
     }
 }
